8484a5e2e6514e163100f323bdf4725dc3a8cf5d
copeas <1031848053@qq.com>
2018-12-06 15:24:08 +0800

    xiao gai yi xie wenjian

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,66 @@
-// gulp 的插件;
 
-// 1. http插件; (服务器插件);
-// gulp connect;
+
 const gulp = require("gulp");
-// gulp 服务器插件;
+
 const connect = require("gulp-connect");
-const proxy = require("http-proxy-middleware");
-// gulp 合并插件;
+
 var concat = require('gulp-concat');
-// gulp 压缩插件;
+
 var uglify = require("gulp-uglify");
-// babel 插件;
+
 var babel = require("gulp-babel");
-// css 插件;
+
 var cleanCss = require("gulp-clean-css");
-// sass 编译插件;
+
 var sass = require("gulp-sass-china");
 
 
-gulp.task("connect",()=>{
+gulp.task('connect', function() {
     connect.server({
-        port:"82",
-	    root:"dist",
-        middleware:function(){
-            return[
-                proxy("/api",{
-                    target:"http://localhost:3000",
-                    pathRewrite:{
-                        '^/api' : '/',  //rewrite path
-                    }
-                })
-            ]
+        port:85,
+        root:"dist/",
+        livereload:true,
+   
+        middleware:function(connect , opt){
+            var Proxy = require('gulp-connect-proxy');
+            opt.route = '/proxy';
+            var proxy = new Proxy(opt);
+            return [proxy];
         }
     })
 });
-// 如何发起一个代理请求 : 
-// localhost:82/proxy/目标;
+
 
 gulp.task("html", ()=>{
     return gulp.src("*.html").pipe(gulp.dest("dist/")).pipe(connect.reload());;
 })
 
 gulp.task("watch", ()=>{
-    gulp.watch("*.html",["html","sass"]);
-   
+    gulp.watch("*html",["html","sass"]);
     gulp.watch("sass/*.scss",["html","sass"]);
-    gulp.watch("script/*.js",["html","script"])
+    gulp.watch("js/*.js",["html","js"])
+    gulp.watch("images/*.*",["html","images"])
+ 
+})
+gulp.task("images",()=>{
+    return gulp.src("images/*.*")
+    .pipe(gulp.dest("dist/images/"))
 })
 
-gulp.task("default",["watch","connect"]);
+//默认自动转到dist文件夹下
+gulp.task("default",["watch","connect","images","js"]);
 
 // script 转存指令;
 
-gulp.task("script", ()=>{
-    return gulp.src(["script/*.js","!script/libs/jquery.js"])
+gulp.task("js", ()=>{
+    return gulp.src(["js/*.js"])
     // .pipe(concat("mian.js"))
-    //.pipe(uglify())
-    .pipe(gulp.dest("dist/script"));
+    // .pipe(uglify())
+    .pipe(gulp.dest("dist/js"));
 })
 
 gulp.task("css", ()=>{
-    return gulp.src(["styles/*.css"])
+    return gulp.src(["css/*.css"])
            .pipe(cleanCss())
            .pipe(gulp.dest("dist/css"))
 })
@@ -71,10 +71,10 @@ gulp.task("sass", () =>{
            .pipe(gulp.dest("dist/css"))
 })
 
-// 编译 ? es6 => es5;
+
 
 gulp.task("es6",()=>{
-    return gulp.src("es6/*.js")
+    return gulp.src("script/es2015/es6.js")
     .pipe(babel({
         presets: ['@babel/env']
     }))
